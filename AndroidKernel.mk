50a2d091602f6eca1db535cf337fbbde29007a99
Shaquib imdad <shaquibxyz@gmail.com>
2021-06-29 07:53:21 +0000

    Add remaining OEM modifications
    Signed-off-by: Shaquib imdad <shaquibxyz@gmail.com>

--- AndroidKernel.mk
+++ AndroidKernel.mk
@@ -49,6 +49,10 @@ KERNEL_CONFIG_OVERRIDE := CONFIG_ANDROID_BINDER_IPC_32BIT=y
 endif
 endif
 
+ifeq ($(VERSION_TYPE),longcheer_factory)
+KERNEL_CONFIG_OVERRIDE += CONFIG_KERNEL_CUSTOM_FACTORY=y
+endif
+
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := $(strip $(TARGET_KERNEL_CROSS_COMPILE_PREFIX))
 ifeq ($(TARGET_KERNEL_CROSS_COMPILE_PREFIX),)
 KERNEL_CROSS_COMPILE := arm-eabi-
@@ -76,11 +80,7 @@ KERNEL_GCC_NOANDROID_CHK := $(shell (echo "int main() {return 0;}" | $(KERNEL_CR
 
 real_cc :=
 ifeq ($(KERNEL_LLVM_SUPPORT),true)
-  ifeq ($(KERNEL_ARCH), arm64)
-    real_cc := REAL_CC=$(KERNEL_LLVM_BIN) CLANG_TRIPLE=aarch64-linux-gnu-
-  else
-    real_cc := REAL_CC=$(KERNEL_LLVM_BIN) CLANG_TRIPLE=arm-linux-gnueabihf
-  endif
+real_cc := REAL_CC=$(KERNEL_LLVM_BIN) CLANG_TRIPLE=aarch64-linux-gnu-
 else
 ifeq ($(strip $(KERNEL_GCC_NOANDROID_CHK)),0)
 KERNEL_CFLAGS := KCFLAGS=-mno-android
@@ -103,17 +103,6 @@ else
     BUILD_ROOT_LOC := ../
     TARGET_KERNEL_SOURCE := kernel
     KERNEL_OUT := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ
-endif
-
-# Add RTIC DTB to dtb.img if RTIC MPGen is enabled.
-# Note: unfortunately we can't define RTIC DTS + DTB rule here as the
-# following variable/ tools (needed for DTS generation)
-# are missing - DTB_OBJS, OBJDUMP, KCONFIG_CONFIG, CC, DTC_FLAGS (the only available is DTC).
-# The existing RTIC kernel integration in scripts/link-vmlinux.sh generates RTIC MP DTS
-# that will be compiled with optional rule below.
-# To be safe, we check for MPGen enable.
-ifdef RTIC_MPGEN
-RTIC_DTB := $(KERNEL_SYMLINK)/rtic_mp.dtb
 endif
 
 KERNEL_CONFIG := $(KERNEL_OUT)/.config
@@ -224,19 +213,12 @@ $(KERNEL_HEADERS_INSTALL): $(KERNEL_OUT)
 			echo $(KERNEL_CONFIG_OVERRIDE) >> $(KERNEL_OUT)/.config; \
 			$(MAKE) -C $(TARGET_KERNEL_SOURCE) O=$(BUILD_ROOT_LOC)$(KERNEL_OUT) $(KERNEL_MAKE_ENV) ARCH=$(KERNEL_ARCH) CROSS_COMPILE=$(KERNEL_CROSS_COMPILE) $(real_cc) oldconfig; fi
 
-# RTIC DTS to DTB (if MPGen enabled;
-# and make sure we don't break the build if rtic_mp.dts missing)
-$(RTIC_DTB): $(INSTALLED_KERNEL_TARGET)
-	stat $(KERNEL_SYMLINK)/rtic_mp.dts 2>/dev/null >&2 && \
-	$(DTC) -O dtb -o $(RTIC_DTB) -b 1 $(DTC_FLAGS) $(KERNEL_SYMLINK)/rtic_mp.dts || \
-	touch $(RTIC_DTB)
-
 # Creating a dtb.img once the kernel is compiled if TARGET_KERNEL_APPEND_DTB is set to be false
-$(INSTALLED_DTBIMAGE_TARGET): $(TARGET_PREBUILT_INT_KERNEL) $(INSTALLED_KERNEL_TARGET) $(RTIC_DTB)
+$(INSTALLED_DTBIMAGE_TARGET): $(TARGET_PREBUILT_INT_KERNEL)
 	$(hide) if [ -d "$(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/dts/vendor/" ]; then \
-			cat $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/dts/vendor/qcom/*.dtb $(RTIC_DTB) > $@; \
+			cat $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/dts/vendor/qcom/*.dtb > $@; \
 		else \
-			cat $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/dts/qcom/*.dtb $(RTIC_DTB) > $@; \
+			cat $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/dts/qcom/*.dtb > $@; \
 		fi
 
 .PHONY: kerneltags
